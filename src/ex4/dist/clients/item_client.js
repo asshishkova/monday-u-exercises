7bb919ab3621a67aef1db143d2a9b6109a395122
Anna Shishkova <asshishkova@gmail.com>
2022-06-14 14:48:28 +0300

    Copy pockemon client

--- src/ex4/dist/clients/item_client.js
+++ src/ex4/dist/clients/item_client.js
@@ -1,1 +1,2 @@
-// Create an ItemClient class here. This is what makes requests to your express server (your own custom API!)
+// Create an ItemClient class here.
+// This is what makes requests to your express server (your own custom API!)
