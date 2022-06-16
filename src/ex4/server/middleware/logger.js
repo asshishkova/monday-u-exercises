bcd9f355c865f2b0d17fc52945374417d63d142a
Anna Shishkova <asshishkova@gmail.com>
2022-06-16 21:00:36 +0300

    Update Readme

--- src/ex4/server/middleware/logger.js
+++ src/ex4/server/middleware/logger.js
@@ -1,7 +1,3 @@
-// module.exports = function logger(req, res, next) {
-//   console.log(`Request [${new Date()}]: \n ${req.method}, ${req.path}`);
-//   next();
-// }
 export function logger(req, res, next) {
   console.log(`Request [${new Date()}]: \n  ${req.method} ${req.path}`);
   next();
