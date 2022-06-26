f8c86906e6facaf8bd4eff36d2fa8697ac6a995a
Anna Shishkova <asshishkova@gmail.com>
2022-06-26 16:03:52 +0300

    Change let status to const status

--- src/ex5/server/middleware/error_handler.js
+++ src/ex5/server/middleware/error_handler.js
@@ -4,7 +4,7 @@ function errorHandler(err, req, res, next) {
     if(res.headersSent){
         return next(err)
     }
-    let status = err.statusCode || 500;
+    const status = err.statusCode || 500;
     res.status(status).json({
         "status": status,
         "error": `${err.message || "Something went wrong"}`
