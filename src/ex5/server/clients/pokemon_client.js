e7d66ba09f71862a2ff44ba2d6677b3c0c11515d
Anna Shishkova <asshishkova@gmail.com>
2022-06-28 20:28:28 +0300

    Do not log error from pokemon api if failed

--- src/ex5/server/clients/pokemon_client.js
+++ src/ex5/server/clients/pokemon_client.js
@@ -29,7 +29,7 @@ class PokemonClient {
   }
 
   handleFailure(error, pokemon) {
-    console.log(`Failed to fetch ${pokemon}: `, error);
+    console.log(`Failed to fetch ${pokemon}.`);
   }
 
   isPokemon(name) {
