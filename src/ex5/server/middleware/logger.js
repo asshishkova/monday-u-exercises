17c0c115b8120427850f737552428697d1f76af9
Anna Shishkova <asshishkova@gmail.com>
2022-06-28 20:45:29 +0300

    Format the log

--- src/ex5/server/middleware/logger.js
+++ src/ex5/server/middleware/logger.js
@@ -1,7 +1,5 @@
 function logger(req, res, next) {
-  console.log(`Request [${new Date()}]:
-              \n  ${req.method} ${req.path}
-              \n  Body ${JSON.stringify(req.body)}`);
+  console.log(`Request [${new Date()}]: \n  ${req.method} ${req.path} \n  Body ${JSON.stringify(req.body)}`);
 
   next();
 }
