5de2bd9ab431fb0d774010c14c18c61a1f81bf27
Anna Shishkova <asshishkova@gmail.com>
2022-06-29 16:43:42 +0300

    Add main window

--- src/ex6/client/src/App.js
+++ src/ex6/client/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import { MainWindow } from "./components/main-window.js";
 
 function App() {
   return (
     <div className="App">
-
+      <MainWindow/>
     </div>
   );
 }
---
+++ src/ex6/client/src/components/main-window.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export function MainWindow() {
+  return (
+    <main className="window">
+    <h1 className="title">TODOOPS</h1>
+    <article className="todos-content">
+      <form id="add-todo">
+        <input type="text" id="new-todo-textbox" autofocus required placeholder="Add your new todo" />
+        <button type="submit" id="add-todo-button" className="btn">+</button>
+      </form>
+      <div id="no-todos-placeholder">
+        <img src="../../public/pointer.png" width="100%" height="100%" alt="Add a new todo"/>
+      </div>
+      <ul id="todos-list">
+      </ul>
+      <footer id="footer" style={{display:'none'}}>
+        <button id="sort-list-button" className="btn">
+          Sort by <i className="fa fa-caret-down"></i>
+        </button>
+        <p id="amount-info"></p>
+        <button id="clear-all-button" className="btn">Clear all</button>
+      </footer>
+    </article>
+  </main>
+  )
+}
