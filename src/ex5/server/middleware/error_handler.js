53bc4eb027a9fb52c442cd8d9c58f162a5806542
Anna Shishkova <asshishkova@gmail.com>
2022-07-03 14:21:44 +0300

    Return res.status explicitly

--- src/ex5/server/middleware/error_handler.js
+++ src/ex5/server/middleware/error_handler.js
@@ -2,10 +2,10 @@ function errorHandler(err, req, res, next) {
     console.log("Received error", err.message);
     console.log("Stacktrace", err.stack);
     if(res.headersSent){
-        return next(err)
+        return next(err);
     }
     const status = err.statusCode || 500;
-    res.status(status).json({
+    return res.status(status).json({
         "status": status,
         "error": `${err.message || "Something went wrong"}`
     });
