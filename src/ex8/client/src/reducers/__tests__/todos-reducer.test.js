360d2384e46190b54cbc056ab8ef4fdc5e33acb7
Anna Shishkova <asshishkova@gmail.com>
2022-07-12 20:24:01 +0300

    Update unit tests to todosReducer

--- src/ex8/client/src/reducers/__tests__/todos-reducer.test.js
+++ src/ex8/client/src/reducers/__tests__/todos-reducer.test.js
@@ -12,8 +12,6 @@ const newItems = [
   {id: 5, text: 'five', isNew: true},
 ];
 
-const allItems = [...oldItems, ...newItems];
-
 test('todosReducer should return the initial state', () => {
   expect(todosReducer(undefined, { type: undefined })).toEqual(
     { items: [] }
@@ -28,40 +26,41 @@ test('todosReducer should set a todo list', () => {
 })
 
 test('todosReducer should add new todos to the existing list', () => {
-  const previousState = { items: oldItems };
+  const previousState = { items: [...oldItems] };
   expect(todosReducer(previousState, addTodos(newItems))).toEqual(
-    { items: allItems }
+    { items: [...oldItems, ...newItems] }
   )
 })
 
-test('todosReducer should not add a new todo if it\'s text already exists', () => {
-  const previousState = { items: oldItems };
+test('todosReducer should not add a new todo if it\'s text already exists but changes isNew to true', () => {
+  const previousState = { items: [...oldItems] };
   expect(todosReducer(previousState, addTodos([{id: 1, text: 'one', isNew: false}]))).toEqual(
-    { items: oldItems }
+    { items: [
+      {id: 1, text: 'one', isNew: true},
+      {id: 2, text: 'two', isNew: false},
+      {id: 3, text: 'three', isNew: false}
+    ] }
   )
 })
 
 test('todosReducer should delete an existing todo', () => {
-  const previousState = { items: newItems };
+  const previousState = { items: [...newItems] };
   expect(todosReducer(previousState, deleteTodo({id: 4, text: 'four', isNew: true}))).toEqual(
     { items: [{id: 5, text: 'five', isNew: true}] }
   )
 })
 
 test('todosReducer should not delete anything if todo does not exist', () => {
-  const previousState = { items: oldItems };
+  const previousState = { items: [...oldItems] };
   expect(todosReducer(previousState, deleteTodo({id: 6, text: 'six', isNew: false}))).toEqual(
     { items: oldItems }
   )
 })
 
 test('todosReducer should mark an old item as new', () => {
-  const previousState = { items: allItems };
+  const previousState = { items: [...newItems] };
   expect(todosReducer(previousState, markOld({id: 5, text: 'five', isNew: true}))).toEqual(
     { items: [
-      {id: 1, text: 'one', isNew: false},
-      {id: 2, text: 'two', isNew: false},
-      {id: 3, text: 'three', isNew: false},
       {id: 4, text: 'four', isNew: true},
       {id: 5, text: 'five', isNew: false},
     ] }
@@ -69,8 +68,8 @@ test('todosReducer should mark an old item as new', () => {
 })
 
 test('todosReducer should not change the list in markOld if todo is old', () => {
-  const previousState = { items: allItems };
+  const previousState = { items: [...oldItems] };
   expect(todosReducer(previousState, markOld({id: 1, text: 'one', isNew: false}))).toEqual(
-    { items: allItems }
+    { items: oldItems }
   )
 })
