90b71ef7a798f950380f0011c4673d8a3b18d63d
Anna Shishkova <asshishkova@gmail.com>
2022-07-13 20:13:06 +0300

    Add snapshots for footer

--- src/ex8/client/src/components/__tests__/todo-element.test.jsx
+++ src/ex8/client/src/components/__tests__/todo-element.test.jsx
@@ -1,7 +1,6 @@
 import renderer from "react-test-renderer";
 import { TodoElement } from "../todo-element.js";
 
-
 const pendingTodo = {id: 1, text: 'do this', status: false, done: null}
 const doneTodo = {id: 2, text: 'already done this', status: true, done: "2022-07-13T12:49:57.000Z"}
 
